557f6af6fb8088f45440bc5cb43a9822ca9afcdf
Mathis Delaunay <delaunaymathis@yahoo.fr>
2018-03-23 00:08:09 +0100

    Adding some contents

--- baseWebSymfony/src/Controller/MainController.php
+++ baseWebSymfony/src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class MainController extends Controller
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="index")
      */
     public function index()
     {
